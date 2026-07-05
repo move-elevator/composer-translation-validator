aeec37519b5a08e9f00b31426b1e7c41548297a9
Konrad Michalik <konrad.michalik@gmx.de>
2026-07-05 16:27:54 +0200

    feat: add standalone PHAR distribution

---
+++ src/Command/StandaloneValidateTranslationCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "composer-translation-validator" Composer plugin.
+ *
+ * (c) 2025-2026 Konrad Michalik <km@move-elevator.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MoveElevator\ComposerTranslationValidator\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * StandaloneValidateTranslationCommand.
+ *
+ * @author Konrad Michalik <km@move-elevator.de>
+ * @license GPL-3.0-or-later
+ */
+class StandaloneValidateTranslationCommand extends Command
+{
+    use TranslationValidationBehavior;
+}
---
+++ tests/src/Command/StandaloneValidateTranslationCommandTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "composer-translation-validator" Composer plugin.
+ *
+ * (c) 2025-2026 Konrad Michalik <km@move-elevator.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MoveElevator\ComposerTranslationValidator\Tests\Command;
+
+use MoveElevator\ComposerTranslationValidator\Command\StandaloneValidateTranslationCommand;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * StandaloneValidateTranslationCommandTest.
+ *
+ * @author Konrad Michalik <km@move-elevator.de>
+ * @license GPL-3.0-or-later
+ */
+#[CoversClass(StandaloneValidateTranslationCommand::class)]
+final class StandaloneValidateTranslationCommandTest extends TestCase
+{
+    public function testExecuteWithValidArguments(): void
+    {
+        $commandTester = new CommandTester(new StandaloneValidateTranslationCommand());
+
+        $commandTester->execute([
+            'path' => [__DIR__.'/../Fixtures/translations/xliff/success'],
+        ]);
+
+        $this->assertStringContainsString('Language validation succeeded.', $commandTester->getDisplay());
+        $this->assertSame(Command::SUCCESS, $commandTester->getStatusCode());
+    }
+
+    public function testExecuteWithErrors(): void
+    {
+        $commandTester = new CommandTester(new StandaloneValidateTranslationCommand());
+
+        $commandTester->execute([
+            'path' => [__DIR__.'/../Fixtures/translations/xliff/fail'],
+        ]);
+
+        $this->assertStringContainsString('Language validation failed', $commandTester->getDisplay());
+        $this->assertSame(Command::FAILURE, $commandTester->getStatusCode());
+    }
+
+    public function testExecuteWithJsonFormat(): void
+    {
+        $commandTester = new CommandTester(new StandaloneValidateTranslationCommand());
+
+        $commandTester->execute([
+            'path' => [__DIR__.'/../Fixtures/translations/xliff/success'],
+            '--format' => 'json',
+        ]);
+
+        $output = json_decode($commandTester->getDisplay(), true);
+        $this->assertIsArray($output);
+        $this->assertSame(0, $output['status']);
+        $this->assertSame(Command::SUCCESS, $commandTester->getStatusCode());
+    }
+}
